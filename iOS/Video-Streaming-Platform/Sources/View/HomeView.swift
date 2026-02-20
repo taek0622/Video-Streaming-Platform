e0e84ea49bfc629d0928434f9c7bb9cb308a5ec2
taek0622 <min4644@gmail.com>
2026-02-21 00:22:30 +0900

    Feature: #1 Implement HomeView
    - 홈 뷰 구현

--- iOS/Video-Streaming-Platform/Sources/View/HomeView.swift
+++ iOS/Video-Streaming-Platform/Sources/View/HomeView.swift
@@ -1,5 +1,28 @@
-        .onAppear {
-            let videoma = VideoManager()
-            Task {
-                await videoma.fetchHomeVideo()
-            }
+//
+//  HomeView.swift
+//  Video-Streaming-Platform
+//
+//  Created by 김민택 on 11/26/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    @StateObject private var viewModel = HomeViewModel(service: VideoManager())
+
+    var body: some View {
+        List(viewModel.videos) { video in
+            VideoCard(video: video)
+                .listRowInsets(.init(top: .zero, leading: .zero, bottom: 8, trailing: .zero))
+        }
+        .listStyle(.plain)
+        .task {
+            await viewModel.loadVideos()
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
