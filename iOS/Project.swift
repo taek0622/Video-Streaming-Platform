ad940f3fcef0daaf398715caa5a87076c5357cab
taek0622 <min4644@gmail.com>
2026-02-20 19:13:18 +0900

    Chore: Add localhost allow
    - 개발환경에서 localhost 허용하도록 변경

--- iOS/Project.swift
+++ iOS/Project.swift
@@ -11,6 +11,23 @@ let project = Project(
             deploymentTargets: .iOS("26.0"),
             infoPlist: .extendingDefault(
                 with: [
+                    // (실기기) 로컬 네트워크 접근 권한 팝업에 사용
+                    "NSLocalNetworkUsageDescription": .string("로컬 개발 서버(동일 Wi-Fi) 연결을 위해 필요합니다."),
+                    // ATS 설정
+                    "NSAppTransportSecurity": .dictionary([
+                        // LAN / IP / .local / unqualified 도메인에 대해 ATS가 로컬 네트워킹을 허용
+                        // e.g, http://192.168.x.x 같은 IP 접속 허용
+                        "NSAllowsLocalNetworking": .boolean(true),
+                        // localhost를 "도메인 예외"로 명시해서 http 허용
+                        "NSExceptionDomains": .dictionary([
+                            "localhost": .dictionary([
+                                "NSTemporaryExceptionAllowsInsecureHTTPLoads": .boolean(true)
+                            ]),
+                            "127.0.0.1": .dictionary([
+                                "NSTemporaryExceptionAllowsInsecureHTTPLoads": .boolean(true)
+                            ])
+                        ])
+                    ]),
                     "UILaunchScreen": [
                         "UIColorName": "",
                         "UIImageName": "",
