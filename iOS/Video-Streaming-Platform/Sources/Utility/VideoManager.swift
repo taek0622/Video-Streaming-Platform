be48f62c498a26deff519af040d4d0e7d9d44595
taek0622 <min4644@gmail.com>
2026-02-24 22:19:24 +0900

    Feature: #3 Add fetchSearchedVideo method
    - 특정 키워드를 포함한 영상을 검색하기 위한 메서드 구현

--- iOS/Video-Streaming-Platform/Sources/Utility/VideoManager.swift
+++ iOS/Video-Streaming-Platform/Sources/Utility/VideoManager.swift
@@ -27,4 +27,26 @@ final class VideoManager: VideoService {
         return result
     }
 
+    func fetchSearchedVideo(keyword: String, sortType: SortType, page: Int) async -> SearchedVideoList {
+        var url = baseURL.appending(path: "search")
+        url.append(queryItems: [
+            URLQueryItem(name: "keyword", value: keyword),
+            URLQueryItem(name: "sort", value: sortType.rawValue), // popular: 인기순, latest: 최신순
+            URLQueryItem(name: "page", value: String(page)), // 기본 1
+            URLQueryItem(name: "size", value: "20") // 기본 10, 최대 50
+        ])
+        let request = URLRequest(url: url)
+        var result = SearchedVideoList(hasNext: false, items: [])
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let decoded = try JSONDecoder().decode(SearchedVideoListDTO.self, from: data).asVideoList()
+            result = decoded
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
+
+        return result
+    }
+
 }
