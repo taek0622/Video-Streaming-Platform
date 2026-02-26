8d5a8271d025c2d173bf690f0c4b595b551116f1
taek0622 <min4644@gmail.com>
2026-02-26 23:34:38 +0900

    Feature: #7 Add HttpMethod
    - 요청에 따른 HTTPMethod를 설정하는 enum 추가

---
+++ iOS/Video-Streaming-Platform/Sources/Network/HttpMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HttpMethod.swift
+//  Video-Streaming-Platform
+//
+//  Created by 김민택 on 2/25/26.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+}
